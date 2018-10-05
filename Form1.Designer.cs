db236c3167e60d40405fc2f0f37d50a7d4f99145
Mahmoud Nady <mnady919@gmail.com>
2018-10-06 01:58:24 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace usdt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.a = new System.Windows.Forms.TextBox();
+            this.b = new System.Windows.Forms.TextBox();
+            this.d = new System.Windows.Forms.TextBox();
+            this.c = new System.Windows.Forms.TextBox();
+            this.ae = new System.Windows.Forms.Button();
+            this.ac = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(123, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(820, 49);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(123, 128);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(820, 49);
+            this.textBox2.TabIndex = 1;
+            // 
+            // a
+            // 
+            this.a.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a.Location = new System.Drawing.Point(123, 239);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(287, 49);
+            this.a.TabIndex = 2;
+            // 
+            // b
+            // 
+            this.b.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b.Location = new System.Drawing.Point(656, 239);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(287, 49);
+            this.b.TabIndex = 3;
+            // 
+            // d
+            // 
+            this.d.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.d.Location = new System.Drawing.Point(656, 318);
+            this.d.Name = "d";
+            this.d.Size = new System.Drawing.Size(287, 49);
+            this.d.TabIndex = 5;
+            // 
+            // c
+            // 
+            this.c.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c.Location = new System.Drawing.Point(123, 318);
+            this.c.Name = "c";
+            this.c.Size = new System.Drawing.Size(287, 49);
+            this.c.TabIndex = 4;
+            // 
+            // ae
+            // 
+            this.ae.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ae.Location = new System.Drawing.Point(33, 396);
+            this.ae.Name = "ae";
+            this.ae.Size = new System.Drawing.Size(180, 64);
+            this.ae.TabIndex = 6;
+            this.ae.Text = "answer";
+            this.ae.UseVisualStyleBackColor = true;
+            this.ae.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ac
+            // 
+            this.ac.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ac.Location = new System.Drawing.Point(726, 396);
+            this.ac.Name = "ac";
+            this.ac.Size = new System.Drawing.Size(217, 64);
+            this.ac.TabIndex = 7;
+            this.ac.Text = "answer chosen";
+            this.ac.UseVisualStyleBackColor = true;
+            this.ac.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft NeoGothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 252);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 32);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "a -";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft NeoGothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(604, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 32);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "b -";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft NeoGothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(604, 331);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 32);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "d -";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft NeoGothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(73, 335);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 32);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "c -";
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(332, 401);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(298, 59);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "change Qustions Mode";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft NeoGothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 32);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Qustion :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft NeoGothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 32);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Answer :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label7.Font = new System.Drawing.Font("Al-Jazeera-Arabic-Light", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(240, 190);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(574, 46);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "ملحوظه : لو اسم الفاعل مذكور فى الجمله الاجابة هتبقي غلط";
+            this.label7.UseCompatibleTextRendering = true;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "لو الافعال شاذه البرنامج هيحل غلط";
+            this.notifyIcon1.BalloonTipTitle = "ثانيه بس";
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label8.Font = new System.Drawing.Font("Al-Jazeera-Arabic-Light", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(332, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(498, 46);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "H,I,W  اكتب الحروف دى كابتل he ,we  اى كلمة غير  ";
+            this.label8.UseCompatibleTextRendering = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 477);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ac);
+            this.Controls.Add(this.ae);
+            this.Controls.Add(this.d);
+            this.Controls.Add(this.c);
+            this.Controls.Add(this.b);
+            this.Controls.Add(this.a);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Grammer Solver U5";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox a;
+        private System.Windows.Forms.TextBox b;
+        private System.Windows.Forms.TextBox d;
+        private System.Windows.Forms.TextBox c;
+        private System.Windows.Forms.Button ae;
+        private System.Windows.Forms.Button ac;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace usdt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (MessageBox.Show("Yes     "+" موجوده فى الجمله ولا لآ لو موجوده دوس "+"used to", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                a.Enabled = true;
+                b.Enabled = true;
+                c.Enabled = true;
+                d.Enabled = true;
+                ac.Enabled = true;
+                ae.Enabled = false;
+                label7.Show();
+            }
+            else
+            {
+                a.Enabled = false;
+                b.Enabled = false;
+                c.Enabled = false;
+                d.Enabled = false;
+
+                ae.Enabled = true;
+                ac.Enabled = false;
+                label7.Hide();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Contains("did") || textBox1.Text.Contains("didn't"))
+            {
+                textBox2.Text = textBox1.Text.Replace("...", " [ Use To ]");
+            }
+            else if (textBox1.Text.Contains("but") || textBox1.Text.Contains("young") || textBox1.Text.Contains("never") || textBox1.Text.Contains("now"))
+            {
+                if (textBox1.Text.Contains("young") && textBox1.Text.Contains("love") && textBox1.Text.Contains("enjoy"))
+                {
+                    textBox2.Text = textBox1.Text.Replace("...", " [ didn't Use To ]");
+                }
+                else if (textBox1.Text.EndsWith("g"))
+                {
+                    textBox2.Text = textBox1.Text.Replace("...", " [ didn't Use To ]");
+                }
+                else if (textBox1.Text.Contains("never"))
+                {
+                    textBox2.Text = textBox1.Text.Replace("...", " [ Used To ]");
+                }
+                else if (textBox1.Text.Contains("now"))
+                {
+                    if (textBox1.Text.Contains("isn't") || textBox1.Text.Contains("doesn't") || textBox1.Text.Contains("don't") || textBox1.Text.Contains("aren't") || textBox1.Text.Contains("haven't") || textBox1.Text.Contains("hasn't") || textBox1.Text.Contains("not"))
+                    {
+                        textBox2.Text = textBox1.Text.Replace("...", " [ Used to ]");
+                    }
+                    else
+                    {
+                        textBox2.Text = textBox1.Text.Replace("...", " [didn't Use to ]");
+                    }
+                }
+                else if (textBox1.Text.Contains("but") && textBox1.Text.Contains("is"))
+                {
+                    textBox2.Text = textBox1.Text.Replace("...", " [didn't Use To ]");
+                }
+                else if (textBox1.Text.Contains("but") && textBox1.Text.Contains("does"))
+                {
+                    textBox2.Text = textBox1.Text.Replace("...", " [ didn't Use To ]");
+                }
+                else if (textBox1.Text.Contains("but") && textBox1.Text.Contains("do"))
+                {
+                    textBox2.Text = textBox1.Text.Replace("...", " [ didn't Use To ]");
+                }
+                else if (textBox1.Text.Contains("but") && textBox1.Text.Contains("are"))
+                {
+                    textBox2.Text = textBox1.Text.Replace("...", " [didn't Use To ]");
+                }
+                else if (textBox1.Text.Contains("young"))
+                {
+                    textBox2.Text = textBox1.Text.Replace("...", " [ Used To ]");
+                }
+
+                else if (textBox1.Text.Contains("ing"))
+                {
+                    textBox2.Text = textBox1.Text.Replace("...", " [ Used for ]");
+                }
+                
+            }
+
+            else if (textBox1.Text.Contains("when"))
+            {
+                textBox2.Text = textBox1.Text.Replace("...", " [ Used to ]");
+            }
+            else if (textBox1.Text.Contains("am") || textBox1.Text.Contains("is") || textBox1.Text.Contains("are") )
+            {
+                if (textBox1.Text.Contains("he") || textBox1.Text.Contains("she") || textBox1.Text.Contains("they") || textBox1.Text.Contains("we") || textBox1.Text.Contains("I"))
+                {
+                    textBox2.Text = textBox1.Text.Replace("...", " [ Used to ]");
+                }
+                else if (textBox1.Text.Contains("ing"))
+                {
+                    textBox2.Text = textBox1.Text.Replace("...", " [ Used for ]");
+                }
+                else
+                {
+                    textBox2.Text = textBox1.Text.Replace("...", "[Used to]");
+                }
+            }
+            else
+            {
+                textBox2.Text = textBox1.Text.Replace("...", " [ used to ]");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Contains("is used for ") || textBox1.Text.Contains("are used for") || textBox1.Text.Contains("am used for"))
+            {
+                if (a.Text.Contains("ing"))
+                {
+                    textBox2.Text = textBox1.Text.Replace("...", a.Text);
+                }
+                else if (b.Text.Contains("ing"))
+                {
+                    textBox2.Text = textBox1.Text.Replace("...", b.Text);
+                }
+                else if (c.Text.Contains("ing"))
+                {
+                    textBox2.Text = textBox1.Text.Replace("...", c.Text);
+                }
+                else if (d.Text.Contains("ing"))
+                {
+                    textBox2.Text = textBox1.Text.Replace("...", d.Text);
+                }
+            }
+
+
+            else if (textBox1.Text.Contains("is used to ") || textBox1.Text.Contains("are used to") || textBox1.Text.Contains("am used to") || textBox1.Text.Contains("not used to"))
+            {
+
+
+                if (textBox1.Text.Contains("he") || textBox1.Text.Contains("she") || textBox1.Text.Contains("they") || textBox1.Text.Contains("I") || a.Text.Contains("we"))
+                {
+                    if (a.Text.Contains("ing"))
+                    {
+                        textBox2.Text = textBox1.Text.Replace("...", a.Text);
+                    }
+                    else if (b.Text.Contains("ing"))
+                    {
+                        textBox2.Text = textBox1.Text.Replace("...", b.Text);
+                    }
+                    else if (c.Text.Contains("ing"))
+                    {
+                        textBox2.Text = textBox1.Text.Replace("...", c.Text);
+                    }
+                    else if (d.Text.Contains("ing"))
+                    {
+                        textBox2.Text = textBox1.Text.Replace("...", d.Text);
+                    }
+                } 
+
+
+
+                else
+                {
+                    if (!a.Text.EndsWith("d") && !a.Text.EndsWith("g") && !a.Text.EndsWith("s"))
+                    {
+                        textBox2.Text = textBox1.Text.Replace("...", a.Text);
+                    }
+                    else if (!b.Text.EndsWith("d") && !b.Text.EndsWith("g") && !b.Text.EndsWith("s"))
+                    {
+                        textBox2.Text = textBox1.Text.Replace("...", b.Text);
+                    }
+                    else if (!c.Text.EndsWith("d") && !c.Text.EndsWith("g") && !c.Text.EndsWith("s"))
+                    {
+                        textBox2.Text = textBox1.Text.Replace("...", c.Text);
+                    }
+                    else if (!d.Text.EndsWith("d") && !d.Text.EndsWith("g") && !d.Text.EndsWith("s"))
+                    {
+                        textBox2.Text = textBox1.Text.Replace("...", d.Text);
+                    }
+                }
+            } 
+        
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (ae.Enabled == false)
+            {
+                ae.Enabled = true;
+                ac.Enabled = false;
+
+                a.Enabled = false;
+                b.Enabled = false;
+                c.Enabled = false;
+                d.Enabled = false;
+                label7.Hide();
+            }
+            else
+            {
+                ae.Enabled = false;
+                ac.Enabled = true;
+
+                a.Enabled = true;
+                b.Enabled = true;
+                c.Enabled = true;
+                d.Enabled = true;
+                label7.Show();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (a.Enabled == true)
+            {
+                notifyIcon1.Visible = true;
+                notifyIcon1.Icon = SystemIcons.WinLogo;
+                notifyIcon1.BalloonTipTitle = "ثانيه بس ! ";
+                notifyIcon1.BalloonTipText = "لو الافعال شاذه الاجابه هتبقي غلط" + 
+                            Environment.NewLine + 
+                            Environment.NewLine;
+                notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+                notifyIcon1.ShowBalloonTip(100);
+            }
+        }
+
+    }
+}
